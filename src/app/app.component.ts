322101444f906879e955cd5b1cd10ad39a095fc8
singhonlappy <singhonlappy@gmail.com>
2024-02-11 04:23:38 +0530

    Next commit where ui has been taken care of and also get the repos and details of repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+
+
+
+import { Component } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { forkJoin } from 'rxjs';
+
+interface UserDetail {
+  avatar_url: string;
+  name: string;
+  bio: string;
+  location: string;
+  twitter_username: string
+  url: string
+
+}
+interface UserRepo {
+  name: string;
+  description: string;
+  languages_url: string
+
+}
+interface UserRepoLanguages {
+  name: string;
+  description: string;
+  languages_url: string
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent {
+  Object: any;
+  constructor(private apiService: ApiService) { }
 
-  githubUsername: string='';
+  user: string = '';
+  userdetail: UserDetail =
+    {
+      avatar_url: '',
+      name: '',
+      bio: '',
+      location: '',
+      twitter_username: '',
+      url: '',
 
-  onsubmit()
-{
-  this.apiService.getUser(this.githubUsername).subscribe(console.log);
-}
+    }; // Initialize with empty string or default value
+  userrepo: UserRepo[] = []
+  userrepolanguages: object | any;
 
- 
+  onSubmit() {
+    this.apiService.getUser(this.user).subscribe((data: UserDetail | any) => {
+      // Assign the data to the userdetail object
+      this.userdetail = data;
+      console.log('User Detail:', this.userdetail);
+    });
+    this.apiService.getUserrepo(this.user).subscribe((data: UserRepo | any) => {
+      // Assign the data to the userrepo object
+      this.userrepo = data;
+      console.log('User Repo:', this.userrepo);
+      // Fetch languages for each repository
+
+
+    });
+    
+
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, forkJoin, tap, throwError } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,12 @@ export class ApiService {
 
   getUser(githubUsername: string) {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  }
+  getUserrepo(githubUsername: string) {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`);
+  }
+  getUserrepolanguages(githubUsername: string, repoName: string): Observable<any> {
+    return this.httpClient.get(`https://api.github.com/repos/${githubUsername}/${repoName}/languages`);
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
